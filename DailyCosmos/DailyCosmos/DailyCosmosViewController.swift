f34589a32ddcb60e2bb32fa6b2f480c3e7ef8350
stnrr <manjeet.singh@airblack.co>
2022-05-08 17:33:11 +0530

    (feat): add UI elements and outlets to view controller

---
+++ DailyCosmos/DailyCosmos/DailyCosmosViewController.swift
@@ -0,0 +1,22 @@
+//
+//  DailyCosmosViewController.swift
+//  DailyCosmos
+//
+//  Created by Manjeet Singh on 08/05/22.
+//
+
+import UIKit
+
+class DailyCosmosViewController: UIViewController {
+    
+    @IBOutlet private weak var titleLabel: UILabel!
+    @IBOutlet private weak var imageView: UIImageView!
+    @IBOutlet private weak var descriptionView: UITextView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+}
+
