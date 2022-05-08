53e2b1d93f282b0e3a61ca48c138334c669da494
stnrr <manjeet.singh@airblack.co>
2022-05-08 21:10:17 +0530

    (feat): add cosmos api manager

---
+++ DailyCosmos/DailyCosmos/DailyCosmosManager.swift
@@ -0,0 +1,32 @@
+//
+//  DailyCosmosManager.swift
+//  DailyCosmos
+//
+//  Created by Manjeet Singh on 08/05/22.
+//
+
+import Foundation
+
+class DailyCosmosManager: NSObject {
+    func fetchDailyCosmosData(completion: @escaping ((Result<DailyCosmosModel, Error>) -> Void)) {
+        if let url = URL(string: "https://api.nasa.gov/planetary/apod?api_key=ak1mBQTAJL2tYkv0pcrhRgFSYUiRZduiUBDQr6Gj") {
+            var request = URLRequest(url: url)
+            request.httpMethod = "GET"
+            request.setValue("application/json", forHTTPHeaderField: "Accept")
+
+            let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
+                if let jsonData = data {
+                    do {
+                        let decoder = JSONDecoder()
+                        let cosmosData = try decoder.decode(DailyCosmosModel.self, from: jsonData)
+                        completion(.success(cosmosData))
+                    } catch {
+                        print(error.localizedDescription)
+                        completion(.failure(error))
+                    }
+                }
+            }
+            task.resume()
+        }
+    }
+}
